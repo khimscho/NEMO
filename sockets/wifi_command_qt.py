f09689851654eddff38485ea58964ef42b1f9139
Brian Calder <brc@ccom.unh.edu>
2020-07-01 23:04:48 -0400

    Bug fixes for binary file transfer mode for TCP terminal test application.

--- sockets/wifi_command_qt.py
+++ sockets/wifi_command_qt.py
@@ -10,62 +10,57 @@ import time
 
 class CaptureReturn(QObject):
     messageReady = pyqtSignal(str)
+    
     def __init__(self):
         QObject.__init__(self)
-        self.binary_mode = False
-        self.binary_filename = ""
-
-    @pyqtSlot()
-    def set_binary_mode(self, flag):
-        self.binary_mode = flag
-        print("Binary transfer mode set to: " + flag)
-        
-    @pyqtSlot()
-    def set_binary_filename(self, name)
-        self.binary_filename = name
-        print("Binary filename set to: " + name)
         
     @pyqtSlot()
     def capture(self):
+        global binary_mode
+        global server_sock
         while True:
-            first_byte = server_sock.recv(1)
-            if self.binary_mode:
-                # In binary mode, we can't grab sentences and emit them as they come, since there aren't natural breaks
-                # and in any case we don't want to spam the text record with random binary data.  The goal here is to
-                # report how many bytes are expected, show the first few packets, and then confirm that it's done.  We
-                # rely on the logger sending a length word as the first piece of data.
-                transfer_size_buffer = first_byte + server_sock.recv(3)
-                transfer_size = int.from_bytes(transfer_size_buffer, "little")
-                buffer = "Binary transfer: " + transfer_size + " bytes to " + self.binary_filename
-                self.messageReady.emit(buffer)
-                f = open(self.binary_filename, 'wb')
-                bytes_transferred = 0
-                for b in range(transfer_size):
-                    in_byte = server_sock.recv(1)
-                    f.write(in_byte)
-                    ++bytes_transferred
-                f.close()
-                buffer = "Transferred " + bytes_transferred + " bytes total"
-                self.messageReady.emit(buffer)
-            else:
-                # In non-binary mode, the data that we're going to get is just sentences of ASCII text, and therefore
-                # we gather data into a buffer until we see a newline, and then emit for the record.  We get a chance
-                # to convert modes at the end of each sentence.
-                buffer = first_byte.decode("utf8")
-                sentence_complete = False
-                while not sentence_complete:
-                    in_byte = server_sock.recv(1)
-                    ch = in_byte.decode("utf8")
-                    if ch != '\r':
-                        buffer += ch
-                    if ch == '\n':
-                        sentence_complete = True
-                self.messageReady.emit(buffer)
+            try:
+                first_byte = server_sock.recv(1)
+                if binary_mode:
+                    # In binary mode, we can't grab sentences and emit them as they come, since there aren't natural breaks
+                    # and in any case we don't want to spam the text record with random binary data.  The goal here is to
+                    # report how many bytes are expected, show the first few packets, and then confirm that it's done.  We
+                    # rely on the logger sending a length word as the first piece of data.
+                    transfer_size_buffer = first_byte + server_sock.recv(3)
+                    transfer_size = int.from_bytes(transfer_size_buffer, "little")
+                    buffer = "Binary transfer: " + str(transfer_size) + " bytes to \"" + binary_filename + "\".\n"
+                    self.messageReady.emit(buffer)
+                    f = open(binary_filename, 'wb')
+                    bytes_transferred = 0
+                    for b in range(transfer_size):
+                        in_byte = server_sock.recv(1)
+                        f.write(in_byte)
+                        bytes_transferred += 1
+                    f.close()
+                    buffer = "Transferred " + str(bytes_transferred) + " bytes total.\n"
+                    self.messageReady.emit(buffer)
+                else:
+                    # In non-binary mode, the data that we're going to get is just sentences of ASCII text, and therefore
+                    # we gather data into a buffer until we see a newline, and then emit for the record.  We get a chance
+                    # to convert modes at the end of each sentence.
+                    buffer = first_byte.decode("utf8")
+                    sentence_complete = False
+                    while not sentence_complete:
+                        in_byte = server_sock.recv(1)
+                        ch = in_byte.decode("utf8")
+                        if ch != '\r':
+                            buffer += ch
+                        if ch == '\n':
+                            sentence_complete = True
+                    self.messageReady.emit(buffer)
+            except BlockingIOError:
+                pass
+            except:
+                e = sys.exc_info()[0]
+                print("Exception: " + str(e))
 
 
 class Window(QtWidgets.QMainWindow):
-    binary_mode = pyqtSignal(boolean)
-    binary_filename = pyqtSignal(str)
     
     def __init__(self):
         super(Window, self).__init__()
@@ -74,28 +69,35 @@ class Window(QtWidgets.QMainWindow):
 
         # We start the capture thread that grabs output from the server, and buffers for display
         self.obj = CaptureReturn()
-        self.binary_mode.connect(self.obj.set_binary_mode)
-        self.binary_filename.connect(self.obj.set_binary_filename)
-        self.binary_mode.emit(False)
         self.rx_thread = QThread()
         self.obj.messageReady.connect(self.outputMessageReady)
         self.obj.moveToThread(self.rx_thread)
         self.rx_thread.started.connect(self.obj.capture)
         self.rx_thread.start()
 
+    @pyqtSlot()
     def doCommand(self):
+        global server_sock
         cmd = self.lineEdit.text()
         self.lineEdit.setText("")
-        self.updateRecord("\n$ " + cmd + "\n")
-        if cmd.startswith("transfer"):
-            self.binary_mode.emit(True)
-            file_number = cmd.split(' ')[1]
-            file_name = 'nmea2000.' + file_number
-            self.binary_filename.emit(file_name)
+        self.updateRecord("$ " + cmd + "\n")
+        if cmd.startswith("binary"):
+            mode = cmd.split(' ')[1]
+            if mode == "on":
+                set_transfer_mode(True)
+            else:
+                set_transfer_mode(False)
         else:
-            self.binary_mode.emit(False)
-        server_sock.send(cmd.encode("utf8"))
+            if cmd.startswith("transfer"):
+                set_transfer_mode(True)
+                file_number = cmd.split(' ')[1]
+                file_name = 'nmea2000.' + file_number
+                set_transfer_filename(file_name)
+            else:
+                set_transfer_mode(False)
+            server_sock.send(cmd.encode("utf8"))
 
+    @pyqtSlot(str)
     def outputMessageReady(self, message):
         self.updateRecord(message)
 
@@ -103,11 +105,21 @@ class Window(QtWidgets.QMainWindow):
         self.textBrowser.setText(self.textBrowser.toPlainText() + message)
         self.textBrowser.verticalScrollBar().setValue(self.textBrowser.verticalScrollBar().maximum())
 
-
+def set_transfer_mode(flag):
+    global binary_mode
+    binary_mode = flag
+    
+def set_transfer_filename(name):
+    global binary_filename
+    binary_filename = name
+    
 server_address = "192.168.4.1"
 server_port = 25443
-print("Starting connection for logger on " + server_address + " port " + server_port)
+print("Starting connection for logger on " + str(server_address) + " port " + str(server_port))
 server_sock = socket.create_connection((server_address, server_port))
+
+binary_mode = False
+binary_filename = ""
 
 app = QtWidgets.QApplication([])
 win = Window()
