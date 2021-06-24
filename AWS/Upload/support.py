8ee405cca5e0751e2211f08bd521528aa9164a9f
Brian Calder <brc@ccom.unh.edu>
2021-06-24 14:26:32 -0400

    Merged development branch into master.

---
+++ AWS/Upload/support.py
@@ -0,0 +1,27 @@
+import boto3
+import uuid
+
+
+def create_bucket_name(bucket_prefix):
+    # The generated bucket name must be between 3 and 63 chars long
+    return ''.join([bucket_prefix, str(uuid.uuid4())])
+
+
+def create_bucket(bucket_prefix, s3_connection):
+    session = boto3.session.Session()
+    current_region = session.region_name
+    bucket_name = create_bucket_name(bucket_prefix)
+    bucket_response = s3_connection.create_bucket(
+        Bucket=bucket_name,
+        CreateBucketConfiguration={
+        'LocationConstraint': current_region})
+    print(bucket_name, current_region)
+    return bucket_name, bucket_response
+
+
+def create_temp_file(size, file_name, file_content):
+    random_file_name = ''.join([str(uuid.uuid4().hex[:6]), file_name])
+    with open(random_file_name, 'w') as f:
+        f.write(str(file_content) * size)
+    return random_file_name
+
