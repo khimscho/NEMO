551fd559e26559097eec6a0ae20f879156475b65
Brian Miles <selimnairb@gmail.com>
2022-05-09 16:11:34 -0400

    Add --version option

--- wibl-python/wibl/cmd/__main__.py
+++ wibl-python/wibl/cmd/__main__.py
@@ -1,6 +1,7 @@
 import sys
 import argparse
 
+from wibl import __version__ as version
 from wibl.cmd.edit_wibl_file import editwibl
 
 
@@ -15,10 +16,13 @@ class WIBL:
         editwibl   Edit WIBL logger files, e.g., add platform metadata.
                 '''
         )
+        parser.add_argument('--version', help='print version and exit',
+                            action='version', version=f"wibl-python {version}")
         parser.add_argument('command', help='Subcommand to run')
-        # TODO: Add version argument.
+
         # Only consume the command argument here, let sub-commands consume the rest
         args = parser.parse_args(sys.argv[1:2])
+
         if not hasattr(self, args.command):
             print(f"Unrecognized command: {args.command}")
             parser.print_help()
