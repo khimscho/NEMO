5cd99c77c15d53390503ce814f3839197f336410
Brian Calder <brc@ccom.unh.edu>
2023-02-01 09:38:50 -0500

    Added copyright statements and file-level comments to REST API prototype.

--- wibl-python/wibl/manager/application.py
+++ wibl-python/wibl/manager/application.py
@@ -1,3 +1,36 @@
+##\file application.py
+# \brief Main application code for a Flask-based RESTful interface to a metadata database
+#
+# Once data is being uploaded into the WIBL cloud segment, the Trusted node (and potentially
+# data collectors) will want to know that the files have been correctly processed, and then
+# uploaded.  This code builds a Flask-based application that exposes a REST API for update
+# and management of the metadata in a SQLite database, so that the processing and upload code
+# can provide information on the files being manipulated, and another website can be used to
+# front this information for the Trusted Node, and provide manipulation tools.  This is
+# intentionally very minimal, since we want it to run on as limited a resource as possible.
+#
+# Copyright 2023 Center for Coastal and Ocean Mapping & NOAA-UNH Joint
+# Hydrographic Center, University of New Hampshire.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+
 from flask import Flask, abort
 from flask_restful import Api, Resource, reqparse, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
--- wibl-python/wibl/manager/run_tests.py
+++ wibl-python/wibl/manager/run_tests.py
@@ -1,3 +1,33 @@
+##\file run_tests.py
+# \brief Simple (and non-complete) testing for REST API for database management
+#
+# This code is intended to be used primarily to exercise the database REST API during
+# development, and does not constitute a complete regression test environment for
+# the system.  It will, however, do interesting things to the database, which should
+# allow for new systems to be checked.
+#
+# Copyright 2023 Center for Coastal and Ocean Mapping & NOAA-UNH Joint
+# Hydrographic Center, University of New Hampshire.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+
 import requests
 import uuid
 
