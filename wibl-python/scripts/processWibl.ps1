17658b32adb07633cc82dd4ffbe7f365b500fcb9
bmiles_ccom <bmiles@ccom.unh.edu>
2023-10-11 15:01:07 -0400

    wibl-python: scripts: Add error handling to example script

--- wibl-python/scripts/processWibl.ps1
+++ wibl-python/scripts/processWibl.ps1
@@ -11,10 +11,18 @@ Get-ChildItem -Path $wiblPath -Filter *.wibl -File -Recurse |
     $editedName = $_ -replace '\.wibl', '-edited.wibl'
     Write-Output "Edited file name: $editedName"
     Write-Output "Running command: 'wibl editwibl -m $metadataFile $_ $editedName'..."
-    #wibl editwibl -m $metadataFile $_ $editedName
+    wibl editwibl -m $metadataFile $_ $editedName
+    if ( -not ($LASTEXITCODE -eq 0) ) {
+      Write-Error "Error running wibl 'editwibl', exiting..."
+      exit -1
+    }
     # Run wibl procwibl to create GeoJSON output
     $processedName = $_ -replace '\.wibl', '.geojson'
     Write-Output "GeoJSON file name: $processedName"
     Write-Output "Running command: 'wibl procwibl -c $wiblConfig $editedName $processedName'..."
-    #wibl procwibl -c $wiblConfig $editedName $processedName
+    wibl procwibl -c $wiblConfig $editedName $processedName
+    if ( -not ($LASTEXITCODE -eq 0) ) {
+      Write-Error "Error running 'wibl procwibl', exiting..."
+      exit -1
+    }
   }
