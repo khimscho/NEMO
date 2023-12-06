7afa88eae481070a4262b528d6efe400343fc1f0
bmiles_ccom <bmiles@ccom.unh.edu>
2023-12-06 12:18:28 -0500

    wibl-python: data preview: GMT zsh script: add inset

--- wibl-python/doc/viz/wibl-plot-gmt.bash
+++ wibl-python/doc/viz/wibl-plot-gmt.bash
@@ -7,8 +7,9 @@ GEBCO_FILE=$3
 
 COLOR_TABLE="${SOUNDINGS_FILE}.cpt"
 FORMATS='pdf,png'
-# TODO: make dynamic based on scale of soundings
+# TODO: make bounds buffer dynamic based on scale of soundings
 BOUNDS_BUFFER=0.5
+INSET_BUFFER_INC=$((4*$BOUNDS_BUFFER-$BOUNDS_BUFFER))
 # TODO: allow width to be specified
 PROJECTION=M6i
 
@@ -28,14 +29,27 @@ gdalinfo -json $SOUNDINGS_FILE | \
     @sh "GMT_PLOT_YMAX=$((\(.wgs84Extent.coordinates[0][0][1]) + $BOUNDS_BUFFER))"
   ' | tee | while read cmd; do eval "export $cmd"; done
 REGION="$GMT_PLOT_XMIN/$GMT_PLOT_XMAX/$GMT_PLOT_YMIN/$GMT_PLOT_YMAX"
+# Calculate region for overview inset map
+GMT_PLOT_XMIN_INSET=$(($GMT_PLOT_XMIN - $INSET_BUFFER_INC))
+GMT_PLOT_XMAX_INSET=$(($GMT_PLOT_XMAX + $INSET_BUFFER_INC))
+GMT_PLOT_YMIN_INSET=$(($GMT_PLOT_YMIN - $INSET_BUFFER_INC))
+GMT_PLOT_YMAX_INSET=$(($GMT_PLOT_YMAX + $INSET_BUFFER_INC))
+REGION_INSET="$GMT_PLOT_XMIN_INSET/$GMT_PLOT_XMAX_INSET/$GMT_PLOT_YMIN_INSET/$GMT_PLOT_YMAX_INSET"
+REGION_POLY="${NAME_STEM}-inset.txt"
+cat << EOF > $REGION_POLY
+$GMT_PLOT_XMIN $GMT_PLOT_YMIN $GMT_PLOT_XMAX $GMT_PLOT_YMAX
+EOF
 
 # Plot
 gmt begin ${NAME_STEM} ${FORMATS}
   gmt grdimage ${GEBCO_FILE} -J$PROJECTION -R$REGION -Cterra
-  gmt colorbar -DJBC+o0c/0.8c -Bx100+l'GEBCO Bathymetry' -By+lm
-  # TODO: Change +w100k (width of scalebar in km) to be dynamic based on extent
+  gmt colorbar -DJBC+o0c/0.8c -Bx50+l'GEBCO Bathymetry' -By+lm
+  # TODO: Change +w50k (width of scalebar in km) to be dynamic based on extent
   #   see if gmtmath is useful for this: https://docs.generic-mapping-tools.org/6.4/gmtmath.html
   gmt basemap -J$PROJECTION -R$REGION -B -LjBR+o0.75c/0.5c+w100k+f+u
   gmt grdimage ${SOUNDINGS_FILE} -C${COLOR_TABLE}
-  # TODO: Add inset
+  gmt inset begin -DjTR+w2c/2.2c+o0.25c/0.25c -F+gwhite+p1p+c0.1c
+    gmt coast -R$REGION_INSET -J'M2c' -Swhite -Ggrey --MAP_FRAME_TYPE=plain
+    gmt plot $REGION_POLY -J'M?' -Sr+s -W0.5p,blue
+  gmt inset end
 gmt end
