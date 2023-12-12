9dfb3054e3cd9eefde052fb50064dc260956cec5
bmiles_ccom <bmiles@ccom.unh.edu>
2023-12-12 15:30:09 -0500

    wibl-python: data preview: filter soundings greater than reasonable values; plot soundings with a color map; add colorbar for soundings; make map bounding box scaling automatic

--- wibl-python/doc/viz/pygmt_test_gtiff.py
+++ wibl-python/doc/viz/pygmt_test_gtiff.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pygmt
 from osgeo import gdal
 import rasterio
@@ -5,20 +7,26 @@ import rasterio
 gdal.UseExceptions()
 
 RASTER_NODATA = -99999.
-RASTER_RES = 0.0005
+RASTER_MAX = 40_000
+RASTER_RES = 0.0009
+REGION_INSET_MULT = 4
 
 gebco_path: str = 'data/gebco_2023/GEBCO_2023.nc'
-buffer = 0.5
-buffer_inset = 4 * buffer
-sounding_path: str = 'data/bigfile-md.json'
-sounding_rast: str = 'data/bigfile-md.tif'
+# sounding_str: str = 'data/bigfile-md.json'
+sounding_str: str = 'data/pnw-b12_2.0.json'
+sounding_path: Path = Path(sounding_str)
+sounding_rast: Path = sounding_path.with_suffix('.tif')
+
+# TODO: Merge GeoJSON into a single file
+# $ ogrmerge.py -o ~/Downloads/GeoJSON/merged.json ~/Downloads/GeoJSON/*.json -single
+
 
 # Generate GeoTIFF of soundings from GeoJSON file
 try:
     gdal.Rasterize(sounding_rast, sounding_path, format='GTiff',
                    outputSRS='EPSG:4326', xRes=RASTER_RES, yRes=-RASTER_RES,
                    noData=RASTER_NODATA, creationOptions=['COMPRESS=DEFLATE', 'ZLEVEL=9'],
-                   attribute='depth')
+                   attribute='depth', where=f"depth < {RASTER_MAX}")
 except Exception as e:
     # TODO: Handle this exception once we move this to library code
     pass
@@ -29,6 +37,13 @@ with rasterio.open(sounding_rast) as d:
     xmax = d.bounds.right
     ymin = d.bounds.bottom
     ymax = d.bounds.top
+
+# Buffer map bounds around data based on the maximum of the x- and y-ranges
+#   to avoid extremely tall or wide maps
+xrng = xmax - xmin
+yrng = ymax - ymin
+buffer = max(xrng, yrng) / 2
+buffer_inset = REGION_INSET_MULT * buffer
 
 # Setup region, projections and polygons for main map an insets...
 region = [xmin - buffer,
@@ -51,10 +66,11 @@ center_lon: float = (xmin + xmax) / 2
 center_lat: float = (ymin + ymax) / 2
 global_inset_proj = f"G{center_lon}/{center_lat}/?"
 
-pygmt.config(FONT_TITLE='20p,Helvetica')
+pygmt.config(FONT_TITLE='20p,Helvetica',
+             FONT_LABEL='12p,Helvetica')
 f = pygmt.Figure()
 
-title = f"Soundings from '{sounding_path}'"
+title = f"Soundings from '{sounding_str}'"
 f.basemap(region=region,
           projection='M16c',
           frame=["afg", f"+t{title}"])
@@ -67,7 +83,10 @@ f.grdimage(gebco_path,
 f.colorbar(position="JBC", frame=["x+lGEBCO 2023 Bathymetry", "y+lm"])
 
 # Plot soundings
-f.grdview(sounding_rast, cmap=True)
+f.grdimage(sounding_rast, cmap='haxby', nan_transparent=True)
+f.colorbar(position="JRM+o-2.0c/0c+w4c",
+           box="+gwhite@30+p0.8p,black",
+           frame=["x+lSounding depth", "y+lm"])
 
 # Plot global inset
 with f.inset(
@@ -96,5 +115,4 @@ with f.inset(
     )
     f.plot(data=region_poly, style="r+s", pen="0.5p,blue")
 
-# f.savefig('pygmt_test_gtiff.png')
-f.psconvert(prefix='pygmt_test_gtiff', fmt='f', resize='+m0.2c')
+f.psconvert(prefix=sounding_path.stem, fmt='f', resize='+m0.2c')
